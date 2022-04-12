f0de38e5192c77ff60e269860d85d74f36615162
yandys09 <yandys03@naver.com>
2022-04-12 21:10:23 +0900

    NavBar Menu added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./Header";
 
 const App = () => (
-  <div className="App">
-    <p>Inside App Component</p>
-  </div>
+  <BrowserRouter>
+    <Header />
+  </BrowserRouter>
 );
 
 export default App;
