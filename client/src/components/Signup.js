112b8bd31b3c95742c355c3c6956b43238932d8d
yandys09 <yandys03@naver.com>
2022-04-15 21:25:38 +0900

    error & succes Message added

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,60 +1,143 @@
-import React from "react";
+import React, { useState } from "react";
+
+import isEmail from "validator/lib/isEmail";
+import isEmpty from "validator/lib/isEmpty";
+import equals from "validator/lib/equals";
+import { showErrorMsg, showSuccessMsg } from "../helpers/message";
+import { showLoading } from "../helpers/loading";
+
 import "./Signup.css";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+    successMsg: false,
+    errorMsg: false,
+    loading: true,
+  });
+
+  const {
+    username,
+    email,
+    password,
+    password2,
+    successMsg,
+    errorMsg,
+    loading,
+  } = formData;
+
+  const handleChange = (evt) => {
+    // console.log(evt)
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
+      successMsg: "",
+      errorMsg: "",
+    });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    //client-side validation
+    if (
+      isEmpty(username) ||
+      isEmpty(email) ||
+      isEmpty(password) ||
+      isEmpty(password2)
+    ) {
+      setFormData({
+        ...formData,
+        errorMsg: "All fields are required",
+      });
+    } else if (!isEmail(email)) {
+      setFormData({
+        ...formData,
+        errorMsg: "Invalid email",
+      });
+    } else if (!equals(password, password2)) {
+      setFormData({
+        ...formData,
+        errorMsg: "Passwords do not match!!",
+      });
+    } else {
+      //success
+      setFormData({
+        ...formData,
+        successMsg: "Validation success!!!",
+      });
+    }
+
+    //  console.log(formData)
+  };
+
   const showSignupForm = () => (
-    <form className="signup-form">
+    <form className="signup-form" onSubmit={handleSubmit} noValidate>
       <div className="form-group input-group">
         <div className="input-group-prepend">
           <span className="input-group-text">
-            <i class="bi bi-person-fill"></i>
+            <i className="bi bi-person-fill"></i>
           </span>
         </div>
         <input
-          name=""
+          name="username"
+          value={username}
           className="form-control"
           placeholder="UserName"
           type="text"
+          onChange={handleChange}
         />
       </div>
 
       <div className="form-group input-group">
         <div className="input-group-prepend">
           <span className="input-group-text">
-            <i class="bi bi-envelope-fill"></i>
+            <i className="bi bi-envelope-fill"></i>
           </span>
         </div>
         <input
-          name=""
+          name="email"
+          value={email}
           className="form-control"
           placeholder="Email address"
           type="email"
+          onChange={handleChange}
         />
       </div>
 
       <div className="form-group input-group">
         <div className="input-group-prepend">
           <span className="input-group-text">
-            <i class="bi bi-lock-fill"></i>
+            <i className="bi bi-lock-fill"></i>
           </span>
         </div>
         <input
+          name="password"
+          value={password}
           className="form-control"
           placeholder="Create password"
           type="password"
+          onChange={handleChange}
         />
       </div>
 
       <div className="form-group input-group">
         <div className="input-group-prepend">
           <span className="input-group-text">
-            <i class="bi bi-unlock-fill"></i>
+            <i className="bi bi-unlock-fill"></i>
           </span>
         </div>
         <input
+          name="password2"
+          value={password2}
           className="form-control"
           placeholder="Confirm password"
           type="password"
+          onChange={handleChange}
         />
       </div>
 
@@ -66,16 +149,21 @@ const Signup = () => {
 
       {/* already have account */}
       <p className="text-center text-white">
-        Have an account?<a href="#">Log In</a>
+        Have an account?<Link to="/signin">Log In</Link>
       </p>
     </form>
   );
+
   return (
     <div className="signup-container">
       <div className="row px-3 vh-100">
         <div className="col-md-5 mx-auto align-self-center">
-          
+          {successMsg && showSuccessMsg(successMsg)}
+          {errorMsg && showErrorMsg(errorMsg)}
+          {loading && <div className="text-center pb-4"> {showLoading()}</div>}
           {showSignupForm()}
+
+          {/* <p style={{ color: "red" }}>{JSON.stringify(formData)}</p> */}
         </div>
       </div>
     </div>
